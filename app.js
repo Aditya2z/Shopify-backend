fa7df989e7e08fed8e2f92900bbe9bec9c5e18a7
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2024-01-01 00:37:12 +0530

    bug fix

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const cartRouter = require("./routes/cart");
 const app = express();
 app.use(cors());
 
+console.log("Mongouri",process.env.Mongo_Uri);
+console.log(process.env);
+
 mongoose
   .connect(process.env.Mongo_Uri)
   .then(() => {
